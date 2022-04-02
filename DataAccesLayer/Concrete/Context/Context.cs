70b24d58da6d446a23a4c133e3571039b1f2f787
yuyag <yuyag@DESKTOP-9FLRJ7J>
2022-04-03 02:03:57 +0300

    Add project files.

---
+++ DataAccesLayer/Concrete/Context/Context.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccesLayer.Concrete
+{
+    public class Context : DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-9FLRJ7J; database=CoreBlogDb; integrated security=true;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments{ get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ DataAccesLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccesLayer.Abstract;
+using DataAccesLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        public void AddCategory(Category category)
+        {
+            c.Add(category);
+            c.SaveChanges();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            c.Remove(category);
+            c.SaveChanges();
+        }
+
+        public Category GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

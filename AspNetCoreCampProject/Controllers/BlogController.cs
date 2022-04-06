2e0e5e9aba79e05be2eab6037fdf200055ffc23c
Yagmur <yu.yagmursahin8@gmail.com>
2022-04-06 17:45:45 +0300

    Adding the theme to the project, listing blogs, editing the read more and date format, blog read more page and other improvements

---
+++ AspNetCoreCampProject/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using BussinesLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreCampProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+        public IActionResult BlogReadAll(int id)
+        {
+        var values = bm.GelBlogByID(id);
+            return View(values);
+        }
+    }
+}
---
+++ AspNetCoreCampProject/Controllers/CommentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreCampProject.Controllers
+{
+	public class CommentController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public PartialViewResult PartialViewComment()
+		{
+			return PartialView();
+		}
+		public PartialViewResult CommentListByBlog()
+		{
+			return PartialView();
+		}
+	}
+}
